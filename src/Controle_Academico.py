a2759d7a79b7dbc4aa32b64e7588669a343c7a8b
Rafael Barbosa Martins <20241CECA0387@cefetes.br>
2025-06-13 21:37:56 -0300

    varius shit

--- src/Controle_Academico.py
+++ src/Controle_Academico.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+
+st.sidebar.title("Sistema de Controle Acadêmico")
 st.title("Projeto de Implementação - Sistema de Controle Acadêmico")
 
 st.header("Desenvolvido por:")
--- src/functions/create_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def create_db():
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS disciplinas (
-            codigo TEXT PRIMARY KEY,
-            
