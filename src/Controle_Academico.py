8137296f4a21681837a023b7d2023ace7572991e
Rafael Barbosa Martins <91491788+RafaBMartins@users.noreply.github.com>
2025-06-17 21:00:13 -0300

    Delete src directory

--- src/Controle_Academico.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-st.sidebar.title("Sistema de Controle Acadêmico")
-st.title("Projeto de Implementação - Sistema de Controle Acadêmico")
-
-st.header("Desenvolvido por:")
-st.text("Rafael Martins\nGabriel Medeiros")
